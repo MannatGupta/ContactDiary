7d543913d46cf3ed9682da4139c08d3b30a56531
Mannat Gupta <mannatgupta@Mannats-MacBook-Air.local>
2023-11-21 22:26:56 +0530

    search bar added

--- contactDiary/src/App.jsx
+++ contactDiary/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { RiUserSearchFill as SearchContactIcon } from "react-icons/ri";
+import { LuUserPlus as AddContactIcon } from "react-icons/lu";
 import Navbar from './components/navbar'
 
 const App = () => {
   return (
-    <div className='max-w-[370px] mx-auto'>
-     <Navbar/>
+    <div className='max-w-[370px] mx-auto px-4'>
+      <Navbar/>
+      <div className='flex gap-2'>
+      <div className='flex relative items-center ml-1'>
+        <SearchContactIcon className='text-white text-3xl absolute \'/>
+        <input type='text' className='bg-transparent border pl-[116px] text-white flex-grow border-white rounded-md h-10'/>
+      </div>
+      <AddContactIcon className='text-white text-3xl cursor-pointer'/>
+      </div>
     </div>
   )
 }
--- contactDiary/src/components/Navbar.jsx
+++ contactDiary/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { BiGridAlt as ContactDiaryLogo} from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <div className='h-[60px] bg-white m-4 text-xl font-medium rounded-lg flex justify-center items-center gap-2'>
+    <div className='h-[60px] bg-white my-4 text-xl font-medium rounded-lg flex justify-center items-center gap-2'>
         <span><ContactDiaryLogo/></span>
-        <h1>Navbar 2</h1>
+        <h1>Contact Diary</h1>
     </div>
         
   )
